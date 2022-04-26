2c25f8a66466253d6284aad197c5eaa779f27b7f
YuMing Hsueh <yuming.hsueh@outlook.com>
2022-04-26 23:58:13 +0800

    Looks good

---
+++ client/pages/index.js
@@ -0,0 +1,8 @@
+const style = {
+    wrapper: `flex justify-center h-screen w-screen select-none bg-[#15202b] text-white`,
+    content: `max-w-[1400px] w-2/3 flex justify-between`,
+}
+
+export default function Home() {
+    
+}
