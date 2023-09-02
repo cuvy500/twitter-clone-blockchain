270be9b3bb7c104ade6f2153e69efc38710cf60b
Cuvy Jet <cuvy500@gmail.com>
2023-09-02 08:50:13 +0800

    Change line 22 root to x_root

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ ReactDOM.render(
       </NotificationProvider>
     </MoralisProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('x_root')
 );
 
 // If you want to start measuring performance in your app, pass a function
